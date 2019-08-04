c8879a5ace96b02081431c38a6b8d177613b482e
Gabriel Mello <gabrielmello757@gmail.com>
2019-08-03 21:23:29 -0300

    now more functional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-const soma = (a, b) => a + b;
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { render } from 'react-dom';
 
-alert(soma(7, 3))
+class Button extends Component {
+    static defaultProps = {
+        children: 'Guardar',
+    };
+    
+    static propTypes = {
+        onClick: PropTypes.func.isRequired,
+        children: PropTypes.string,
+    };
+
+    render() {
+        return (
+        <a href="" onClick={this.props.onClick}>
+         {this.props.children}
+        </a>
+        );
+    }
+}
+
+class App extends Component {
+    handleClick(){
+        alert("Opa, tudo salvo");
+    }
+
+    render() {
+        return (
+        <Fragment>
+         <h1>Eae Seus Marvado, Tudo Emriba?</h1> 
+         <Button onClick={this.handleClick}>Bora Guardar Esse Progresso</Button>
+        </Fragment> 
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
